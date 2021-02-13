b4fde4cd534b51d527114b7fa6385137fc1ad05b
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-13 15:30:46 +0100

    done

---
+++ pyramid_generic.js
@@ -0,0 +1,10 @@
+// La solution attendue
+let c = '*'
+console.log('${c}                 ${c}')
+console.log('${c}${c}              ${c}${c}')
+console.log('${c}${c}${c}           ${c}${c}${c}')
+console.log('${c}${c}${c}${c}        ${c}${c}${c}${c}')
+console.log('${c}${c}${c}${c}${c}     ${c}${c}${c}${c}${c}')
+console.log('${c}${c}${c}${c}${c}${c}   ${c}${c}${c}${c}${c}${c}')
+console.log('${c}${c}${c}${c}${c}${c}${c} ${c}${c}${c}${c}${c}${c}${c}')
+console.log('${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}${c}')
